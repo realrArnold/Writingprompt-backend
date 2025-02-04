db28a2504cb17a4197809c3d688698639194a13d
Vanessa Bizzell <vanessabizzell@gmail.com>
2025-02-04 18:15:40 +0000

    user authentication token storage added

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const app = express(); 
+const app = express();
 const port = process.env.PORT || 3001;
 const router = require("./router");
 const dotenv = require("dotenv");
+const { v4: uuidv4 } = require("uuid");
 const connectDB = require("./config/database");
+const User = require("./schemas/User");
+const cors = require("cors");
 
 dotenv.config();
 
@@ -11,9 +14,42 @@ connectDB();
 
 app.use(express.json());
 app.use(router);
+app.use(cors());
 
+//this gets the token so you have ID for the bouncer to check...
+//where does route link to?
+app.post("/events/auth", async (req, res) => {
+  console.log(req.body.username);
+  console.log("arrived");
+  const user = await User.findOne({ username: req.body.username });
+  console.log(user);
+  if (!user) {
+    //can use a library for error
+    return res.sendStatus(401);
+  }
+  if (req.body.password !== user.password) {
+    return res.sendStatus(403);
+  }
+  //uuid is a library that generates random strings
+  user.token = uuidv4();
+  await user.save();
+  res.send({ token: user.token });
+});
 
+// Authorization middleware - this is the 'bouncer' checking your ID before entry!
+// HAS TO BE BEFORE ROUTER STUFF
+app.use(async (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  console.log(authHeader);
+  const user = await User.findOne({ token: authHeader });
+  console.log(user);
+  if (user) {
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-    })
+  console.log(`Server is running on port ${port}`);
+});
