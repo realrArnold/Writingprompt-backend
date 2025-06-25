abc580b75b9640bd1745f6608786373b467b5be6
Ryan Arnold <ryanarnoldcontact@gmail.com>
2025-06-25 12:40:19 +0100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
 try {  
-    const connection = await mongoose.connect(process.env.MONGODB_URI)
+    const connection = await mongoose.connect(process.env.MONGODB_URL)
     console.log(`Connected to db ${connection.connection.host}`)
 } catch (error) {
 console.error(`Error: ${error.message}`);
